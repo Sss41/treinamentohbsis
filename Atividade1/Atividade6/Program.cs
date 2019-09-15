58a25713648a240c36dde2653bb664a23322a867
Sss41 <silvanasil.1@hotmail.com>
2019-09-15 15:59:30 -0300

    Commitativi6

--- Atividade1/Atividade6/Program.cs
+++ Atividade1/Atividade6/Program.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atividade5
+namespace Atividade6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insira um texto: ");
-            int quantidade = texto.Length();
-
-            Console.WriteLine($"tamanho do texto inserido: {texto.Length}");
+            Console.WriteLine("Insira um texto contendo a palavra banana.");
+            var texto1 = Console.ReadLine();
+            texto1  = texto1.Replace("banana", "Laranja");
+            Console.WriteLine(texto1);
             Console.ReadKey();
         }
     }
