dc25072e3ed10316375fbaa6ad5568d123103e05
Sss41 <silvanasil.1@hotmail.com>
2019-09-15 13:41:37 -0300

    Commmitativ3.2

--- Atividade1/Atividade3/Program.cs
+++ Atividade1/Atividade3/Program.cs
@@ -11,31 +11,23 @@ namespace Atividade3
         static void Main(string[] args)
         {
 
-            Console.WriteLine(" Qual seu nome?");
-
-            string nomecompleto = Console.ReadLine();
-
-            Console.WriteLine($" {nomecompleto}");
-            Console.ReadLine();
-
-
-            Console.WriteLine(" Qual sua idade?");
+            Console.WriteLine("Qual seu nome completo?");
+            string nomecompleto = Console.ReadLine();// variavel recebe o texto acima
             
-            int idade = int.Parse (Console.ReadLine());
+            Console.WriteLine("Qual sua idade?");
+            int idade = 0;//variavel recebe idade que usuaria ira digitar
+            idade = int.Parse(Console.ReadLine());
+                
             if (idade >= 18)
             {
                 Console.WriteLine($" Parabéns você é adulto");
                 Console.ReadLine();
-
             }
-
             else
             {
-              Console.WriteLine("Calma logo você será adulto!");
+              Console.WriteLine($"Calma {nomecompleto} logo você será adulto! ");
               Console.ReadKey();
-
             }
-
         }
     }
 }
---
+++ Atividade1/Atividade4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insira um texto: ");
+            var texto = Console.ReadLine();
+            int quantidade = texto.Length;
+            char pricaracter = ' ', ultimocaracter = ' ';
+            int a = 0;
+            for (int i = 0; i < quantidade; i++)
+            {
+                if (i == 0)//se for a primeira letra, salva na primeira
+                {
+                    pricaracter = texto[i];
+                }
+                a = i;
+            }
+            ultimocaracter = texto[a];
+            Console.WriteLine($"o primeiro caracter é '{pricaracter}' e o ultimo é: '{ultimocaracter}'");
+            Console.ReadKey();
+        }
+    }
+}
