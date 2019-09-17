6252a5379c359418c40c9c8be8c2f812e9469f1f
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-17 10:21:16 -0300

    foreach2/17/9

--- Solution1/ForeachNaLista/Program.cs
+++ Solution1/ForeachNaLista/Program.cs
@@ -10,15 +10,13 @@ namespace ForeachNaLista
     {
         static void Main(string[] args)
         {
-            ListaDateTime(); 
+            ListaDateTime();
+            Console.ReadKey();
         }
         /// <summary>
         /// Mostra lista de datas definidas
         /// </summary>
         private static void ListaDateTime()
-            ///sumary
-            ///mostra lista de string definidas
-            ////sumary
         {
             var minhaLista = new List<DateTime>();
 
@@ -26,10 +24,13 @@ namespace ForeachNaLista
             minhaLista.Add(new DateTime(2019, 9, 18));
             minhaLista.Add(new DateTime(2019, 9, 19));
             minhaLista.Add(new DateTime(2019, 9, 20));
-            minhaLista.ForEach(minhaData => Console.WriteLine(minhaData));
-            Console.ReadKey();
+            minhaLista.ForEach(minhaData => Console.WriteLine(minhaData.ToLongDateString()));
+            
 
         }
+        ///sumary
+        ///mostra lista de string definidas
+        ////sumary
         private static void ListaString()
         {
             var minhaLista = new List<string>();
@@ -40,7 +41,30 @@ namespace ForeachNaLista
             
             minhaLista.ForEach(i => Console.WriteLine(i));
             
-            Console.ReadKey();
+            
+        }
+        private static void ListaInteiro()
+        {
+            var minhaLista = new List<int>();
+            minhaLista.Add(17);
+            minhaLista.Add(18);
+            minhaLista.Add(19);
+            minhaLista.Add(20);
+
+            minhaLista.ForEach(meuNumero => Console.WriteLine(meuNumero));
+
+           
+        }
+        private static void ListaDeDecimais()
+        {
+            var minhaLista = new List<double>();
+
+            minhaLista.Add(3.75);
+            minhaLista.Add(4.8);
+            minhaLista.Add(2.42);
+            minhaLista.Add(0.05);
+            minhaLista.ForEach(meuDecimal => Console.WriteLine(meuDecimal));
         }
     }
+  
 }
