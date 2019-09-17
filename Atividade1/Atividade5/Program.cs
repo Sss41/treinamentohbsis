979c0032928aaef34fa6e4b3940ed0932668cc25
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-17 08:20:24 -0300

    Commit17/9

--- Atividade1/Atividade5/Program.cs
+++ Atividade1/Atividade5/Program.cs
@@ -11,7 +11,8 @@ namespace Atividade5
         static void Main(string[] args)
         {
             Console.WriteLine("Insira um texto: ");
-            int quantidade = texto.Length();
+            var texto = Console.ReadLine();
+            int quantidade = texto.Length;
 
             Console.WriteLine($"tamanho do texto inserido: {texto.Length}");
             Console.ReadKey();
