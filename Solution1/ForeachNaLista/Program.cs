fc40de5f3f20e956f4e9512a4b11a74b8fcbdc3c
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-17 13:37:57 -0300

    Convertendo moeda com lista

--- Solution1/ForeachNaLista/Program.cs
+++ Solution1/ForeachNaLista/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace ForeachNaLista
@@ -10,7 +11,11 @@ namespace ForeachNaLista
     {
         static void Main(string[] args)
         {
-            ListaDateTime();
+            // ListaDeNumerosReais();
+            //ListaNumerosEuro();
+            // ListaNumerosYen();
+            ListaNumerosBitCoin();
+
             Console.ReadKey();
         }
         /// <summary>
@@ -25,7 +30,7 @@ namespace ForeachNaLista
             minhaLista.Add(new DateTime(2019, 9, 19));
             minhaLista.Add(new DateTime(2019, 9, 20));
             minhaLista.ForEach(minhaData => Console.WriteLine(minhaData.ToLongDateString()));
-            
+
 
         }
         ///sumary
@@ -38,10 +43,10 @@ namespace ForeachNaLista
             minhaLista.Add("IPHONE APPLE");
             minhaLista.Add("SAMSUNG");
             minhaLista.Add("NOKIA");
-            
+
             minhaLista.ForEach(i => Console.WriteLine(i));
-            
-            
+
+
         }
         private static void ListaInteiro()
         {
@@ -53,8 +58,9 @@ namespace ForeachNaLista
 
             minhaLista.ForEach(meuNumero => Console.WriteLine(meuNumero));
 
-           
+
         }
+        /// minha lista de decimais
         private static void ListaDeDecimais()
         {
             var minhaLista = new List<double>();
@@ -65,6 +71,67 @@ namespace ForeachNaLista
             minhaLista.Add(0.05);
             minhaLista.ForEach(meuDecimal => Console.WriteLine(meuDecimal));
         }
+        private static void ListaDeNumerosReais()
+        {
+            var minhaLista = new List<double>();
+
+            minhaLista.Add(3.75);
+            minhaLista.Add(4.8);
+            minhaLista.Add(2.42);
+            minhaLista.Add(0.05);
+            minhaLista.ForEach(meuDecimal => Console.WriteLine(meuDecimal.ToString("C") + ""+ FormataNumeroDecimaEmDolar(meuDecimal)));
+        }
+        private static string FormataNumeroDecimaEmDolar(double meuNumero)
+        {
+            return (meuNumero / 4.5008).ToString("C",CultureInfo.CreateSpecificCulture("en-US"));
+        }
+        private static void ListaNumerosEuro()
+        {
+
+            var minhaLista = new List<double>();
+
+            minhaLista.Add(3.75);
+            minhaLista.Add(4.8);
+            minhaLista.Add(2.42);
+            minhaLista.Add(0.05);
+            minhaLista.ForEach(meuDecimal => Console.WriteLine(meuDecimal.ToString("C") + " " + FormataNumeroDecimaEmEuro(meuDecimal)));
+        }
+        private static string FormataNumeroDecimaEmEuro(double meuNumero)
+        {
+            return (meuNumero / 5.12).ToString("C", CultureInfo.CreateSpecificCulture("en-GB"));
+        }
+        private static void ListaNumerosYen()
+        {
+
+            var minhaLista = new List<double>();
+
+            minhaLista.Add(3.75);
+            minhaLista.Add(4.8);
+            minhaLista.Add(2.42);
+            minhaLista.Add(0.05);
+            minhaLista.ForEach(meuDecimal => Console.WriteLine(meuDecimal.ToString("C") + " " + FormataNumeroDecimaEmYen(meuDecimal)));
+        }
+        private static string FormataNumeroDecimaEmYen(double meuNumero)
+        {
+           return(meuNumero / 5.12).ToString("C", CultureInfo.CreateSpecificCulture("ja-JP"));
+        }
+
+        private static void ListaNumerosBitCoin()
+        {
+
+            var minhaLista = new List<double>();
+
+            minhaLista.Add(3.75);
+            minhaLista.Add(4.8);
+            minhaLista.Add(2.42);
+            minhaLista.Add(0.05);
+            minhaLista.ForEach(meuDecimal => Console.WriteLine(meuDecimal.ToString("C") + " " + FormataNumeroDecimaEmBitCoin(meuDecimal)));
+        }
+        private static string FormataNumeroDecimaEmBitCoin(double meuNumero)
+        {
+            return (meuNumero /41897.97).ToString("C7", CultureInfo.CreateSpecificCulture("en-US"));
+        }
     }
-  
+
+
 }
