d4470042435b5f7d404a4b794cc92e86a95ccaa6
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-17 09:17:25 -0300

    usando foreach na lista

---
+++ Solution1/ForeachNaLista/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForeachNaLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDateTime(); 
+        }
+        /// <summary>
+        /// Mostra lista de datas definidas
+        /// </summary>
+        private static void ListaDateTime()
+            ///sumary
+            ///mostra lista de string definidas
+            ////sumary
+        {
+            var minhaLista = new List<DateTime>();
+
+            minhaLista.Add(new DateTime(2019, 9, 17));
+            minhaLista.Add(new DateTime(2019, 9, 18));
+            minhaLista.Add(new DateTime(2019, 9, 19));
+            minhaLista.Add(new DateTime(2019, 9, 20));
+            minhaLista.ForEach(minhaData => Console.WriteLine(minhaData));
+            Console.ReadKey();
+
+        }
+        private static void ListaString()
+        {
+            var minhaLista = new List<string>();
+            minhaLista.Add("XIAOMI");
+            minhaLista.Add("IPHONE APPLE");
+            minhaLista.Add("SAMSUNG");
+            minhaLista.Add("NOKIA");
+            
+            minhaLista.ForEach(i => Console.WriteLine(i));
+            
+            Console.ReadKey();
+        }
+    }
+}
