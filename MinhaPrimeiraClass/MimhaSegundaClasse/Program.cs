9357effc2a0cc4a70a7ae06eac8b05eff04f8179
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-18 17:32:14 -0300

    Classes

---
+++ MinhaPrimeiraClass/MimhaSegundaClasse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MimhaSegundaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaCestsDeFrutas = new List<Frutas>();
+
+            minhaCestaDeFruttas.Add(new Frutas()
+            {
+                Nome = "Banana",
+                Quantidade = 5
+            });
+
+        }
+    }
+}
---
+++ MinhaPrimeiraClass/MinhaPrimeiraClass/Model/CestaDeFrutas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraClass.Model
+{
+    class CestaDeFrutas
+    {
+          public string nome  { get; set; }
+          public int quantidade { get; } = 2;
+    }
+}
