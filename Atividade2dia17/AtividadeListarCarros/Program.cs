9510fe72a5df1409568b92a8c8a87931d39be59e
Sss41 <silvanasil.1@hotmail.com>
2019-09-17 23:53:19 -0300

    Atividade4 Listar carros

---
+++ Atividade2dia17/AtividadeListarCarros/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeListarCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaCarros();
+        }
+        private static void ListaCarros()
+        {
+            var minhaLista = new List<string>();
+            Console.WriteLine("Informe a marca do carro:");
+            minhaLista.Add(Console.ReadLine());
+
+            Console.WriteLine("Informe outra marca do carro:");
+            minhaLista.Add(Console.ReadLine());
+
+            Console.WriteLine("Informe outra marca do carro:");
+            minhaLista.Add(Console.ReadLine());
+
+            Console.WriteLine("Informe outra marca do carro:");
+            minhaLista.Add(Console.ReadLine());
+
+            Console.WriteLine("Informe outra marca do carro:");
+            minhaLista.Add(Console.ReadLine());
+
+            //            minhaLista.Add("Fiat.");
+            //            minhaLista.Add("Renault.");
+            //            minhaLista.Add("Chevrolet.");
+            //            minhaLista.Add("Peugeout.");
+
+
+            minhaLista.ForEach(i => Console.WriteLine(i));
+            Console.ReadKey();
+        }
+    }
+}
