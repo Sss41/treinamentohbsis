33cb1068f297acfb79af824bd162cc5120d624b4
Sss41 <silvanasil.1@hotmail.com>
2019-09-17 22:46:28 -0300

    Calculando área quadrado

---
+++ Atividade2dia17/Atividade2dia17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2dia17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculoDeAreaQuadrado();
+            
+        }
+        private static void calculoDeAreaQuadrado()
+        {
+           
+
+            Console.WriteLine("Informe o tamanho de um dos lados do quadrado:");
+            var lado = double.Parse(Console.ReadLine());
+            //double area = lado * lado;
+            Console.WriteLine($"A área do quadrado é:{lado * lado}");
+            Console.ReadKey();
+           
+
+        }
+
+    }
+}
