26f2c9038b1d74fb4fb6a6ef98aa7c001ef60e56
Sss41 <silvanasil.1@hotmail.com>
2019-09-17 21:54:02 -0300

    Segundaarvore

---
+++ calculadora/Exercicosdia17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicosdia17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("        (*)");
+            Console.WriteLine("       (***)");
+            Console.WriteLine("       (***)");
+            Console.WriteLine("      (*****)");
+            Console.WriteLine("      (******)         ");
+            Console.WriteLine("     (********)       ");
+            Console.WriteLine("     (*********)      ");
+            Console.WriteLine("    (***********)     ");
+            Console.WriteLine("    (************)");
+            Console.WriteLine("        |     |");
+            Console.WriteLine("        |     |");
+            Console.WriteLine("        |     |");
+            Console.WriteLine("        |     |");
+            Console.WriteLine(@"      /      \");
+            Console.ReadKey();
+        }
+    }
+}
