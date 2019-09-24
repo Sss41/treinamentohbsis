cb2b9868b35550bef7f414d5c0e4aba73ef6f02c
F34R1337 <filipeeduardoregis2017@gmail.com>
2019-09-24 09:58:43 -0300

    Aula dia 24 parte 1

---
+++ ListandoPessoas2/ListandoPessoas2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListandoPessoas2.Controller;
+using ListandoPessoas2.Model;
+
+namespace ListandoPessoas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PessoaController pessoaController
+              
+                
+                = new PessoaController();
+
+
+            pessoaController.ListaDePessoasPublica.ForEach(i => MostrandoInformacoes(i));
+
+
+            
+            Console.ReadKey();
+        }
+        private static void MostrandoInformacoes(Pessoa i)
+        {
+            string template = "Id {0,3} Nome {1,10}";
+
+            string textoFormatado = string.Format(template, i.Id, i.Nome);
+
+            Console.WriteLine(textoFormatado);
+        }   
+    }
+}
