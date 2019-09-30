12eb296148c01a268f518d72fe66fefc4e684640
franz <tmppmrv2@outlook.com>
2019-09-30 17:25:21 -0300

    Sql dia30

---
+++ ListagemDePastas/TestarDllProf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllPastaWin;
+
+
+namespace TestarDllProf
+{
+    class Program
+    {
+        static DocumentsAutoGenerate documentsAutoGenerate = new DocumentsAutoGenerate();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome da pasta a ser criada");
+            documentsAutoGenerate.CriarPastaMeusDocumentos(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Script/dbo.Livros.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Livros] (
+    [Id]               INT         NOT NULL,
+    [Nome]             NCHAR (100) NULL,
+    [Ativo]            BIT         NULL,
+    [ UsuarioCriacao]  INT         DEFAULT ((0)) NULL,
+    [UsuarioAlteracao] INT         DEFAULT ((0)) NULL,
+    [DataCriacao]      DATETIME    DEFAULT (getdate()) NULL,
+    [DataAlteracao]    DATETIME    DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
