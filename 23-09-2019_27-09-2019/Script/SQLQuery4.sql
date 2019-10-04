8a9d7bc5d0d45fd4047183cd23d5fe9693a07824
franz <tmppmrv2@outlook.com>
2019-10-04 17:22:56 -0300

    aula dia04/10/2019

--- 23-09-2019_27-09-2019/Script/SQLQuery4.sql
+++ 23-09-2019_27-09-2019/Script/SQLQuery4.sql
@@ -1,2 +1,43 @@
-﻿
-select Id, Nome, Login from Usuarios where Ativo =1
+﻿update Marcas set UsuInc=1 where UsuInc=0
+
+
+select * from Marcas where UsuInc=1
+
+select * from Marcas where UsuInc=2
+
+select mar.Nome
+from Marcas mar inner join Usuarios usu on mar.UsuInc = usu.Id
+where usu.Usuario like 'Felipe %'
+
+
+select mar.Nome
+from Marcas mar inner join Usuarios usu on mar.UsuInc = usu.Id
+where usu.Usuario like 'Giomar %'
+
+
+select  Usuarios.Usuario,count (Marcas.UsuInc) as 'Qtde  de marcas'from Marcas  
+inner join Usuarios  on Marcas.UsuInc = Usuarios.Id
+where Usuarios.Usuario  like 'Felipe' group by Usuarios.Usuario	order by  Usuarios.Usuario desc
+
+
+select  Usuarios.Usuario,count (Marcas.UsuInc) as 'Qtde  de marcas'from Marcas  
+inner join Usuarios  on Marcas.UsuInc = Usuarios.Id
+where Usuarios.Usuario  like 'Giomar' group by Usuarios.Usuario	order by  Usuarios.Usuario asc
+
+
+select  Usuarios.Usuario,count (Marcas.UsuInc) as 'Qtde  de marcas'from Marcas  
+inner join Usuarios  on Marcas.UsuInc = Usuarios.Id
+ group by Usuarios.Usuario	order by  Usuarios.Usuario desc
+
+
+ 
+select  Usuarios.Usuario,Carros.Modelo as 'Modelo'from Carros 
+inner join Usuarios  on Carros.UsuInc = Usuarios.Id
+where Usuarios.Usuario  like 'Felipe' order by Usuarios.Usuario desc		
+
+ 
+select  Usuarios.Usuario,Carros.Modelo as 'Modelo'from Carros 
+inner join Usuarios  on Carros.UsuInc = Usuarios.Id
+where Usuarios.Usuario  like 'Giomar' order by Usuarios.Usuario desc		
+
+
---
+++ 23-09-2019_27-09-2019/Script/SQLQuerydia10.sql
@@ -0,0 +1,54 @@
+﻿/*insert into PlayList1 (Nome, Artista,Genero)values('Love on the brain','Rihanna','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values('Shakira','Totura','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values(' Skank','lugar difernte','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values('Anita','Poderosa','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values('Pablo Vittar','Bem na sua cara','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values('Gloria goove','Bum bum de ouro','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values('Karol Konk','È o poder','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values('Isa','Pesdão','Pop')
+insert into PlayList1 (Nome, Artista,Genero)values('Elza Soares','Penha','Samba')
+insert into PlayList1 (Nome, Artista,Genero)values('Beyonce','Turn left','Rock')
+ 
+ Select   Genero,Count(Artista) from Playlist1
+ group by Genero
+
+  
+   select Artista, Count(Nome) from Playlist1
+   group by Artista
+
+   select Nome,Count(Nome) from Playlist1
+   group by Nome*/
+
+
+   insert into genero values ('Pop'),('Samba'),('Rock')
+    select * from Genero
+
+	insert into Artistas (GeneroId,Nome) values('Shakira')
+	insert into Artistas (GeneroId,Nome) values('Elza Soares')
+	insert into Artistas (GeneroId,Nome) values('Beyonce')
+
+	select*from Artistas
+
+	Insert into Cancoes(ArtistasId,Nome)Values('Totura')
+	Insert into Cancoes(ArtistasId,Nome)Values('Turn left')
+	Insert into Cancoes(ArtistasId,Nome)Values('Penha')
+
+select gen.Nome,
+	   art.Nome,  
+	   can.nome from  Genero gen 
+	   inner join Artistas art  on Gen.Id =  art.GeneroId
+	   inner join Cancoes can on  art.Id = can.ARTISTASID
+
+select gen.Nome as 'Genero',
+Count(art. Nome) as 'Quantidade Artistas'
+from Genero gen
+inner join Artistas art on gen.Id = art.GENEROID
+group by gen.Nome-- group by agrupa as tabelas
+
+
+select gen.Nome as 'Genero',
+Count(art. Nome) as 'Quantidade Artistas'
+from Genero gen
+inner join Artistas art on gen.Id = art.GENEROID
+inner join CANCOES can on art.Id = can.ARTISTASID  
+group by gen.Nome
