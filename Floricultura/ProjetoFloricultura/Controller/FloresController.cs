2823c88e4a72ae6687c397e500c34d144d8f79a0
darkeght <darkeght@gmail.com>
2019-10-11 17:31:00 -0300

    aula11flores

---
+++ Floricultura/ProjetoFloricultura/Controller/FloresController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoFloricultura.Model;
+
+namespace ProjetoFloricultura.Controller
+{
+  public  class FloresController
+    {
+        public FloresContextDB contextDb = new FloresContextDB();
+    public IQueryable<Flores> GetFlores()
+    {
+            return contextDb.Flores.Where(x => x.Ativo == true);
+        
+    }
+    }
+
+    
+}
---
+++ Floricultura/ProjetoFloricultura/Model/Flores.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFloricultura.Model
+{
+   public class Flores
+    {
+        [Key]
+       
+
+        public string Id { get; set; }
+        [MaxLength(30)]
+        [Required]
+        public string Nome { get; set; }
+        public bool Quantidade { get; set; } = true;
+        public bool Ativo { get; set; } = true;
+        public string UsuarioCriacao { get; set; }
+        public string UsuarioAlteracao { get; set;}
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+        public DateTime DataAlteracao { get; set; } = DateTime.Now;
+    }
+}
