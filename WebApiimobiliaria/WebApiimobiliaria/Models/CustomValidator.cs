3da557e5057d8e3a1a0552521720381ee4e3a65c
darkeght <darkeght@gmail.com>
2019-10-31 11:45:19 -0300

    31

---
+++ WebApiimobiliaria/WebApiimobiliaria/Models/CustomValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiimobiliaria.Models
+{
+    public class CustomValidator: ValidationAttribute
+    {
+        Model1 db = new Model1();
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (validationContext.DisplayName == "Nome")
+            {
+                if (db.Proprietario.FirstOrDefault(x => x.Nome == value.ToString())
+                != null)
+                    return new ValidationResult("Usuario ja cadastrado");
+            }
+
+
+            if (validationContext.DisplayName == "DataNascimento")
+            {
+                var idade = DateTime.Now.Year - DateTime.Parse(value.ToString()).Year;
+                if (idade <18)
+                    return new ValidationResult("Usuario Sem idade permitida.");
+
+                if (idade >=150)
+                    return new ValidationResult("Vocejá morreu.");
+            }
+            return ValidationResult.Success;
+
+        }
+    }
+}
--- WebApiimobiliaria/WebApiimobiliaria/Models/Proprietario.cs
+++ WebApiimobiliaria/WebApiimobiliaria/Models/Proprietario.cs
@@ -10,13 +10,15 @@ namespace WebApiimobiliaria.Models
     public partial class Proprietario
     {
         public int Id { get; set; }
+        [CustomValidator]
 
         [StringLength(30)]
         public string Nome { get; set; }
-
+        [CustomValidator]
         public DateTime? DataNascimento { get; set; }
 
         [StringLength(80)]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
