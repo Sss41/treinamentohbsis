3a7512d71288ae11b6735941f8d859f23285b61c
darkeght <darkeght@gmail.com>
2019-10-31 17:25:04 -0300

    31.2

--- WebApiBancoExistente1/WebApiBancoExistente1/Controllers/CustomVendas.cs
+++ WebApiBancoExistente1/WebApiBancoExistente1/Controllers/CustomVendas.cs
@@ -29,8 +29,92 @@ namespace WebApiBancoExistente1.Controllers
                                       VendasId = ven.Id,
                                       valor = ven.Quantidade
                                   };
+
             return conteudoRetorno;
         }
 
+
+        [HttpGet]
+        [Route("Api/Carroes/VendasPorAno/{ano}")]
+        public object CustomOnVendasPorAno(int ano)
+        {
+            var listMarcas = db.Vendas.ToList();
+
+            var conteudoRetorno = from ven in listMarcas
+                                  where (ven.DatInc).Year == ano
+
+                                  select new
+                                  {
+                                      VendasId = ven.Id,
+                                      data = ven.DatInc,
+                                      modelo = ven.Quantidade,
+
+                                  };
+
+
+            return conteudoRetorno;
+        }
+
+
+        [HttpGet]
+        [Route("Api/Carroes/Marcas/{ano}/{marca}")]
+        public object CustomOnVendasMarcas(int ano,string marca) 
+        {
+            var listVendas = db.Vendas.ToList();
+            var listUsuario = db.Usuarios.ToList();
+            var listCarro = db.Carros.ToList();
+            var listMarcas = db.Marcas.ToList();
+            var conteudoRetorno = from ven in listVendas
+                                  join usu in listUsuario
+                                  on ven.UsuInc equals usu.Id
+                                  join car in listCarro
+                                  on ven.Carro equals car.Id
+                                  join mar in listMarcas
+                                  on car.Marca equals mar.Id
+                                  where mar.Nome == marca
+                                  where(ven.DatInc).Year == ano
+                            select new
+                              {
+                                 VendasId = ven.Id,
+                                 data = ven.DatInc,
+                                 quantidade=ven.Quantidade,
+                                 modelo= car.Modelo,
+                                 usuario= usu.Usuario1
+
+                              };
+
+
+            return conteudoRetorno;
+        }
+
+        [HttpGet]
+        [Route("Api/Carroes/VendasPorAnoComUsuario/{ano}/{nome}")]
+        public object CustomOnVendasPorAnocomUsuario(int ano, string nome)
+        {
+            var listVendas = db.Vendas.ToList();
+            var listUsuario = db.Usuarios.ToList();
+            var listCarro = db.Carros.ToList();
+            var conteudoRetorno = from ven in listVendas
+                                  join usu in listUsuario
+                                  on ven.UsuInc equals usu.Id
+                                  join car in listCarro
+                                  on ven.Carro equals car.Id
+
+                                  where usu.Usuario1 == nome
+                                  where (ven.DatInc).Year == ano
+
+                                  select new
+                                  {
+                                      VendasId = ven.Id,
+                                      data = ven.DatInc,
+                                      quantidade = ven.Quantidade,
+                                      modelo = car.Modelo,
+                                      usuario = usu.Usuario1
+
+                                  };
+
+
+            return conteudoRetorno;
+        }
     }
 }
